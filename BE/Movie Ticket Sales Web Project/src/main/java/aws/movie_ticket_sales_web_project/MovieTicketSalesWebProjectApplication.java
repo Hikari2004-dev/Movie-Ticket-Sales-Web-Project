a81d6ba50430bbd51735ffdfcf8dbd9eea15faa7
Hikari_Coder <khanhkhoi08@gmail.com>
2025-09-22 23:24:30 +0700

    Insert new springboot project

---
+++ BE/Movie Ticket Sales Web Project/src/main/java/aws/movie_ticket_sales_web_project/MovieTicketSalesWebProjectApplication.java
@@ -0,0 +1,13 @@
+package aws.movie_ticket_sales_web_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieTicketSalesWebProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieTicketSalesWebProjectApplication.class, args);
+    }
+
+}
---
+++ BE/Movie Ticket Sales Web Project/src/test/java/aws/movie_ticket_sales_web_project/MovieTicketSalesWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.movie_ticket_sales_web_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketSalesWebProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
